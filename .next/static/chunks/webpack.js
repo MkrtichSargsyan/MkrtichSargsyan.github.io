e1f4937367428ce38f925f1553fd1ada592404f2
MkrtichSargsyan <mkrtichsargsyan24@gmail.com>
2021-05-23 22:06:23 +0400

    Add basic success message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b15afba6ddbc8152910e";
+/******/ 	var hotCurrentHash = "9515035bcf50252beeef";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/main_content/Contact.js
+++ components/main_content/Contact.js
@@ -5,21 +5,17 @@ import SectionTitle from "../SectionTitle";
 
 export default function Contact() {
   return (
-    <section className={styles.contact_section} id='Contact'>
+    <section className={styles.contact_section} id="Contact">
       <div className="container">
         <SectionTitle title1={"CONTACT"} title2={"Get In Touch"} />
-        <form className={styles.contact} method="post" action="#">
+        {/* <form className={styles.contact} action="https://formsubmit.co/mkrtichsargsyan24@gmail.com" method="POST"> */}
+        <form className={styles.contact} method="POST" action="#">
           <div className={styles.contact_form}>
-            <input
-              name="name"
-              type="text"
-              required=""
-              placeholder="Your Name"
-            />
+            <input name="name" type="text" required placeholder="Your Name" />
             <input
               name="email"
               type="email"
-              required=""
+              required
               placeholder="Your Email"
             />
           </div>
@@ -28,11 +24,19 @@ export default function Contact() {
               name="form-message"
               className="form-control"
               rows="5"
-              required=""
+              required
               placeholder="Your Message..."
             ></textarea>
           </div>
-          <input type="submit" value="Send Message" className={styles.submit_button}/>
+            <input
+              type="submit"
+              value="Send Message"
+              className={styles.submit_button}
+            />
+            <div className ={styles.successMessage}>
+              {/* Thank you for contacting me <br/> */}
+              Your message has been successfully sent
+            </div>
         </form>
       </div>
     </section>
